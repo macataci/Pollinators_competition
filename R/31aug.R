2d7a6a0bdaba5d7dee003baca3e64ec96f4723f8
Maria Camila Tavera <mc.tavera@uniandes.edu.co>
2022-08-31 10:35:45 -0300

    Changes on new model, having P1 death!

--- R/31aug.R
+++ R/31aug.R
@@ -6,26 +6,26 @@ Model <- function(t, y, p) {
   N <- y
   TP <- N[2]+N[3]
   with(as.list(p), {
-    dB.dt <- -Kb*(N[1]^2) + (N[1]*beta*c*(TP/(D+TP)))-d*N[1]
-    dP1.dt <- a[1]*N[1]*(N[2]/TP)*c*(TP/(D+TP)) -((K[1,1]*(N[2]^2))+(K[1,2]*N[2]*N[3]))
-    dP2.dt <-   a[2]*N[1]*(N[3]/TP)*c*(TP/(D+TP)) -((K[2,2]*(N[3]^2))+(K[2,1]*N[2]*N[3]))
+    dB.dt <- beta*N[1]*(1-(N[1]/K[1]))*c*(TP/(D+TP))-(d[1]*N[1])
+    dP1.dt <- a[1]*N[1]*N[2]*(c/(TP+D))*(1-(N[2]*K[2]))-(d[2]*N[2])
+    dP2.dt <-  a[2]*N[1]*N[3]*(c/(TP+D))*(1-(N[3]*K[3]))-(d[3]*N[3])
     return(list(c(dB.dt, dP1.dt, dP2.dt)))
   })
 }
 
+# we can change gammas
 
-#Parameters
+#Parameters Killing P1
 
-beta <- 0.8
-a <- c(0.006, 0.005)
-c <- 50
-Kb <- 0.05
-D <- 20
-K <- matrix(c(0.05, 0.008, 0.1, 0.05), nrow = 2)
-d <- 0.0008
+beta <- 0.005
+a <- c(3.2, 20)
+c <- 20
+D <- 3
+K <- c(10, 0.0981,0.00058)
+d <- c(0.008, 1, 1)
 p <- list(beta, a, c, D, K, d)
-N0 <- c(10, 10, 1000)
-t <- c(1:100)
+N0 <- c(2, 100, 100)
+t <- c(1:1000)
 
 
 out <- ode(y = N0, times = t, func = Model, parms = p)
