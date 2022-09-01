026c40b7e7d6b291f494c9985fcb927fc2276382
MateusSSilva <mateus.souza.2401@gmail.com>
2022-09-01 20:39:39 -0300

    updating the gui code

--- R/Gui_model.R
+++ R/Gui_model.R
@@ -8,8 +8,8 @@ Model <- function(t, y, p) {
   V <- (N[1]*TP)/(Db+TP)
   with(as.list(p), {
     dB.dt <- (beta*V) - (Kb*N[1]^2) - (d[1]*N[1])
-    dP1.dt <- lambda[1]*(V*N[2]/TP)/(Dv+(V*N[2]/TP)) - (K1*N[2]^2) - d[2]*N[2]
-    dP2.dt <- lambda[2]*(V*N[3]/TP)/(Dv+(V*N[2]/TP)) - (K2*N[3]^2) - d[3]*N[3]
+    dP1.dt <- lambda[1]*(V*N[2]/TP)/(Dv+(V*N[2]/TP)) - (K[1]*N[2]^2) - d[2]*N[2]
+    dP2.dt <- lambda[2]*(V*N[3]/TP)/(Dv+(V*N[2]/TP)) - (K[4]*N[3]^2) - d[3]*N[3]
     return(list(c(dB.dt, dP1.dt, dP2.dt)))
   })
 }
@@ -17,17 +17,16 @@ Model <- function(t, y, p) {
 # we can change gammas
 #Parameters
 
-beta <- 0.5
-lambda <- c(0.02, 0.05)
-Db <- 10
-Dv <- 50
-Kb <- 0.0001
-K1 <- 0.00001
-K2 <- 0.00001
-d <- c(0.001, 0.001, 0.001)
-p <- list(beta, lambda, Dv, Db, Kb, K1, K2, d)
-N0 <- c(20, 10, 10)
-t <- c(1:200000)
+Kb <- 0.001
+beta <- 0.1
+lambda <- c(1, 2)
+Dv <- 10
+Db <- 100
+K <-  matrix(c(0.0001, 0, 0, 0.0001), nrow = 2)
+d <- c(0.01, 0.001, 0.001)
+p <- list(Kb, beta, l, Dv, Db, K, d)
+N0 <- c(50, 180, 160)
+t <- c(1:2000)
 
 
 out <- ode(y = N0, times = t, func = Model, parms = p)
@@ -40,51 +39,12 @@ colnames(df) <- c("Time", "Pop", "N")
 ggplot(df) +
   geom_line(aes(x = Time, y = N, color = Pop), size=1.5) +
   labs(x = "Time", y = "N", color = "Species") +
-  theme_classic() +
-  ylim(1,50)
+  theme_classic()
 tail(out)
 
-  
+ggplot(data.frame(out)) +
+  geom_line(aes(x = P1, y = P2), size=1.5) +
+  labs(x = "Plant 1", y = "Plant 2") +
+  theme_classic()
+tail(out)
 
-
-
-
-###########
-a<- par[i]
-
-
-par <- seq(0.01, 0.99, 0.01)
-
-for (i in 1:99) {
-  a<- par[i]
-  #Parameters
-  beta <- 0.5
-  lambda <- c(0.01, 0.05)
-  Db <- 10
-  Dv <- 50
-  Kb <- 0.0001
-  K1 <- 0.00001
-  K2 <- 0.00001
-  d <- c(0.01+a, 0.001, 0.001)
-  p <- list(beta, lambda, Dv, Db, Kb, K1, K2, d)
-  N0 <- c(20, 10, 10)
-  t <- c(1:500000)
-  
-  
-  out <- ode(y = N0, times = t, func = Model, parms = p)
-  out <- as.data.frame(out)
-  colnames(out) <- c("Time", "Bees", "P1", "P2")
-  #View(out)
-  
-  df <- pivot_longer(out, cols = 2:4)
-  colnames(df) <- c("Time", "Pop", "N")
-  ggplot(df) +
-    geom_line(aes(x = Time, y = N, color = Pop), size=1.5) +
-    labs(x = "Time", y = "N", color = "Species") +
-    theme_classic() +
-    ylim(1,50)
-  res <- tail(out, 1)[,2:4]
-  res <- cbind(d[1], res) 
-}
-
-rbind(tail(out, 1), tail(out, 1))
